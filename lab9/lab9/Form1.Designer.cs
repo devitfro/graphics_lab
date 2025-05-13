f59aa7fb0fba8e08b81792e07a53222ab4eecfe2
devitfro <alinabogdanovaaaaa@gmail.com>
2025-05-13 17:14:26 +0300

    update

---
+++ lab9/lab9/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonReflectOX = new System.Windows.Forms.Button();
+            this.buttonScale = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonRotate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Indigo;
+            this.flowLayoutPanel1.Controls.Add(this.buttonRefresh);
+            this.flowLayoutPanel1.Controls.Add(this.buttonReflectOX);
+            this.flowLayoutPanel1.Controls.Add(this.buttonScale);
+            this.flowLayoutPanel1.Controls.Add(this.buttonRotate);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(1041, 45);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(101, 241);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Image = global::lab9.Properties.Resources.refresh;
+            this.buttonRefresh.Location = new System.Drawing.Point(3, 3);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(96, 46);
+            this.buttonRefresh.TabIndex = 4;
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // buttonReflectOX
+            // 
+            this.buttonReflectOX.Font = new System.Drawing.Font("Microsoft YaHei", 24.21818F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReflectOX.ForeColor = System.Drawing.Color.Navy;
+            this.buttonReflectOX.Location = new System.Drawing.Point(3, 55);
+            this.buttonReflectOX.Name = "buttonReflectOX";
+            this.buttonReflectOX.Size = new System.Drawing.Size(96, 56);
+            this.buttonReflectOX.TabIndex = 7;
+            this.buttonReflectOX.Text = "OX";
+            this.buttonReflectOX.UseVisualStyleBackColor = true;
+            this.buttonReflectOX.Click += new System.EventHandler(this.buttonReflectOX_Click);
+            // 
+            // buttonScale
+            // 
+            this.buttonScale.Font = new System.Drawing.Font("Microsoft YaHei", 13.74545F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonScale.ForeColor = System.Drawing.Color.Navy;
+            this.buttonScale.Location = new System.Drawing.Point(3, 117);
+            this.buttonScale.Name = "buttonScale";
+            this.buttonScale.Size = new System.Drawing.Size(96, 56);
+            this.buttonScale.TabIndex = 8;
+            this.buttonScale.Text = "Scale";
+            this.buttonScale.UseVisualStyleBackColor = true;
+            this.buttonScale.Click += new System.EventHandler(this.buttonScale_Click);
+            // 
+            // button3
+            // 
+            this.button3.Image = global::lab9.Properties.Resources.bell_ring;
+            this.button3.Location = new System.Drawing.Point(3, 107);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 46);
+            this.button3.TabIndex = 12;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::lab9.Properties.Resources.thumbtack;
+            this.button1.Location = new System.Drawing.Point(3, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 46);
+            this.button1.TabIndex = 11;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::lab9.Properties.Resources.stars;
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 46);
+            this.button2.TabIndex = 10;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.21818F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.Navy;
+            this.textBox1.Location = new System.Drawing.Point(840, 542);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(301, 49);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.Indigo;
+            this.flowLayoutPanel2.Controls.Add(this.button2);
+            this.flowLayoutPanel2.Controls.Add(this.button1);
+            this.flowLayoutPanel2.Controls.Add(this.button3);
+            this.flowLayoutPanel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(1038, 314);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(103, 156);
+            this.flowLayoutPanel2.TabIndex = 8;
+            // 
+            // buttonRotate
+            // 
+            this.buttonRotate.Font = new System.Drawing.Font("Microsoft YaHei", 13.74545F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonRotate.ForeColor = System.Drawing.Color.Navy;
+            this.buttonRotate.Image = global::lab9.Properties.Resources.rotate_right;
+            this.buttonRotate.Location = new System.Drawing.Point(3, 179);
+            this.buttonRotate.Name = "buttonRotate";
+            this.buttonRotate.Size = new System.Drawing.Size(96, 56);
+            this.buttonRotate.TabIndex = 9;
+            this.buttonRotate.UseVisualStyleBackColor = true;
+            this.buttonRotate.Click += new System.EventHandler(this.buttonRotate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.32727F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(843, 497);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 32);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Enter value to rotate:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1168, 771);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonReflectOX;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button buttonScale;
+        private System.Windows.Forms.Button buttonRotate;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lab9/lab9/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab9
+{
+    public partial class Form1 : Form
+    {
+        private double[,] Sq = new double[4, 3]; // Матриця тіла (4 точки × 3 координати)
+        private double[,] T = new double[3, 3];  // Матриця перетворення (3x3)
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitFigure();     // Початкове положення фігури
+            InitIdentity();   // Одинична матриця
+            this.Paint += Form1_Paint;
+        }
+
+        // початкові координати квадрату
+        private void InitFigure()
+        {
+            Sq[0, 0] = 0; Sq[0, 1] = 0; Sq[0, 2] = 1;
+            Sq[1, 0] = 100; Sq[1, 1] = 0; Sq[1, 2] = 1;
+            Sq[2, 0] = 100; Sq[2, 1] = 100; Sq[2, 2] = 1;
+            Sq[3, 0] = 0; Sq[3, 1] = 100; Sq[3, 2] = 1;
+        }
+
+        // Установка одиничної матриці
+        private void InitIdentity()
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    T[i, j] = (i == j) ? 1 : 0;
+        }
+
+        // Матриця масштабування
+        private void SetScaleMatrix(double scale)
+        {
+            InitIdentity();
+            T[0, 0] = scale;
+            T[1, 1] = scale;
+        }
+
+        // Матриця відображення по осі OX
+        private void SetReflectOX()
+        {
+            InitIdentity();
+            T[1, 1] = -1;
+        }
+
+        // Матриця повороту
+        private void SetRotateMatrix(double angleDegrees)
+        {
+            double angleRad = angleDegrees * Math.PI / 180;
+            InitIdentity();
+            T[0, 0] = Math.Cos(angleRad);
+            T[0, 1] = -Math.Sin(angleRad);
+            T[1, 0] = Math.Sin(angleRad);
+            T[1, 1] = Math.Cos(angleRad);
+        }
+
+        // Застосування матриці T до фігури Sq
+        private void Transform()
+        {
+            double[] b = new double[3];
+
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    b[i] = 0;
+                    for (int k = 0; k < 3; k++)
+                        b[i] += Sq[j, k] * T[k, i];
+                }
+
+                for (int k = 0; k < 3; k++)
+                    Sq[j, k] = b[k];
+            }
+
+            Normalize();
+        }
+
+        private void Normalize()
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Sq[j, 0] /= Sq[j, 2];
+                Sq[j, 1] /= Sq[j, 2];
+                Sq[j, 2] = 1;
+            }
+        }
+      
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen skyBluePen = new Pen(Brushes.DeepSkyBlue);
+            int cx = ClientSize.Width / 2;
+            int cy = ClientSize.Height / 2;
+            g.DrawLine(skyBluePen, cx, 0, cx, ClientSize.Height);
+            g.DrawLine(skyBluePen, 0, cy, ClientSize.Width, cy);
+
+            Pen SqPen = new Pen(Brushes.BlueViolet);
+            g.DrawLine(SqPen, (int)(cx + Sq[0, 0]), (int)(cy - Sq[0, 1]), (int)(cx + Sq[1, 0]), (int)(cy - Sq[1, 1]));
+
+            g.DrawLine(SqPen, (int)(cx + Sq[1, 0]), (int)(cy - Sq[1, 1]), (int)(cx + Sq[2, 0]), (int)(cy - Sq[2, 1]));
+
+            g.DrawLine(SqPen, (int)(cx + Sq[2, 0]), (int)(cy - Sq[2, 1]), (int)(cx + Sq[3, 0]), (int)(cy - Sq[3, 1]));
+
+            g.DrawLine(SqPen, (int)(cx + Sq[3, 0]), (int)(cy - Sq[3, 1]), (int)(cx + Sq[0, 0]), (int)(cy - Sq[0, 1]));
+        }
+
+        private void buttonScale_Click(object sender, EventArgs e)
+        {
+            SetScaleMatrix(2);
+            Transform();
+            Invalidate();
+        }
+
+        private void buttonReflectOX_Click(object sender, EventArgs e)
+        {
+            SetReflectOX();
+            Transform();
+            Invalidate();
+        }
+
+        private void buttonRotate_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double angle))
+            {
+                SetRotateMatrix(angle);
+                Transform();
+                Invalidate();
+            }
+            else
+            {
+                MessageBox.Show("Введіть дійсний кут у градусах.");
+            }
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            InitFigure();
+            Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void bttnArrowRight_Click(object sender, EventArgs e) { }
+
+        private void bttnArrowLeft_Click(object sender, EventArgs e) { }
+
+        private void bttnArrowUp_Click(object sender, EventArgs e) { }
+
+        private void bttnArrowDown_Click(object sender, EventArgs e) { }
+
+        private void bttnRotateRight_Click(object sender, EventArgs e) { }
+
+        private void bttnRotateLeft_Click(object sender, EventArgs e) { }
+
+        private void bttnRefresh_Click(object sender, EventArgs e) { }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+
+        private void button4_Click(object sender, EventArgs e) { }
+    }
+}
